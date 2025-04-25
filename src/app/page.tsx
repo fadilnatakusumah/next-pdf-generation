144859b5944926619cfd28db56f73586f2d797dc
fadilnatakusumah <fadil.ntksmh@gmail.com>
2025-04-25 18:58:33 +0700

    feat: add simple UI frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,102 @@
-import Image from "next/image";
+"use client";
+import { Info } from "lucide-react";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [textValue, setTextValue] = useState("");
+  const [previewURL, setPreviewURL] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  function generate() {
+    if (textValue) {
+      try {
+        new URL(textValue);
+        setPreviewURL(true);
+      } catch (err) {
+        if (err instanceof TypeError) {
+          throw new Error(err.message);
+        }
+        throw new Error("Invalid URL");
+      }
+    }
+  }
+
+  function clearTextValue() {
+    setTextValue("");
+    setPreviewURL(false);
+  }
+
+  return (
+    <main className="container mx-auto py-10 px-4 grid gap-6 md:gap-2 lg:grid-cols-2">
+      <Card className="max-w-2xl h-fit">
+        <CardHeader>
+          <CardTitle>Webpage to PDF Converter</CardTitle>
+          <CardDescription>Generate PDFs from any webpage URL</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              generate();
+            }}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="url">URL</Label>
+                <Input
+                  value={textValue}
+                  id="url"
+                  placeholder="URL of a website"
+                  onChange={({ target }) => setTextValue(target.value)}
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-end gap-2">
+          <Button onClick={clearTextValue} variant="outline">
+            Clear
+          </Button>
+          <Button onClick={generate}>Fetch</Button>
+        </CardFooter>
+      </Card>
+      <Card
+        style={{ scrollbarWidth: "thin" }}
+        className="shadow-md p-4 rounded-lg"
+      >
+        <div className="text-xl flex justify-between items-center mb-4 font-semibold">
+          <div>Preview</div>
+          <Button disabled={!previewURL} color="primary">
+            Generate PDF
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        {previewURL ? (
+          <iframe
+            style={{ scrollbarWidth: "thin" }}
+            src={textValue}
+            className="w-full min-h-screen"
+            title="preview"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        ) : (
+          <div className="flex justify-center items-center h-full gap-2 text-sm">
+            <span>
+              <Info size={16} />
+            </span>
+            <span>You have to fetch a URL first</span>
+          </div>
+        )}
+      </Card>
+    </main>
   );
 }
