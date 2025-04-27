f891632698ecb22fa2d8d6a38b6628fbc381c35e
fadilnatakusumah <fadil.ntksmh@gmail.com>
2025-04-27 10:21:13 +0700

    feat: update meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Next PDF Converter",
+  title: "Next PDF Generator",
   description: "Generate PDFs from any webpage URL",
 };
 
